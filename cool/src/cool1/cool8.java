0e38e92f4bdc84f6dafb60ccd2ca6bc54a98ca20
bharg <bharg@DESKTOP-9JHJ27R>
2021-05-06 16:11:37 +0530

    selenium code

--- cool/src/cool1/cool8.java
+++ cool/src/cool1/cool8.java
@@ -1,11 +1,20 @@
 package cool1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class cool8 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("bhargav");
-		System.out.println("bhargav");
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver.exe");
+		
+		WebDriver driver= new ChromeDriver();
+			
+		driver.get("https://www.google.com/");
+			
+		driver.close();
 
 	}
 

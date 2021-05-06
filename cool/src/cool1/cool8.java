ff88a1e377b6706d1adfb614924825e4fde25d4f
bharg <bharg@DESKTOP-9JHJ27R>
2021-05-06 15:29:04 +0530

    good to o

--- cool/src/cool1/cool8.java
+++ cool/src/cool1/cool8.java
@@ -1,10 +1,21 @@
 package cool1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class cool8 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		
+System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver.exe");
+		
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		
+		driver.close();
 
 	}
 

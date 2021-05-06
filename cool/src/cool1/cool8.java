5a4aacba4133afbd28c7966233106775cad0f4b9
bharg <bharg@DESKTOP-9JHJ27R>
2021-05-06 17:39:52 +0530

    sending

--- cool/src/cool1/cool8.java
+++ cool/src/cool1/cool8.java
@@ -8,13 +8,7 @@ public class cool8 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("bhargav");
-		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver.exe");
 		
-		WebDriver driver= new ChromeDriver();
-			
-		driver.get("https://www.google.com/");
-			
-		driver.close();
 
 	}
 

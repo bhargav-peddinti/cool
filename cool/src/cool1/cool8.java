5290e74730be9b6e005504162d5c85939aa9cf99
bharg <bharg@DESKTOP-9JHJ27R>
2021-05-06 15:55:18 +0530

    push

---
+++ cool/src/cool1/cool8.java
@@ -0,0 +1,12 @@
+package cool1;
+
+public class cool8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("bhargav");
+		System.out.println("bhargav");
+
+	}
+
+}
